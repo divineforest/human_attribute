cf4a7caafd10f0cf05b8814836a592d07f0129ae
Alexander Balashov <divineforest@gmail.com>
2011-11-17 19:28:42 +0400

    Added human_name as alias for self.class.human_attribute_name

--- lib/human_attribute/active_record/base.rb
+++ lib/human_attribute/active_record/base.rb
@@ -1,6 +1,14 @@
 module ActiveRecord
   class Base
 
+    # Looks in en.activerecord.attributes.class.attribute_name
+    # Example I18n lookup: en.activerecord.attributes.user.state
+    def human_name(attribute_name)
+      self.class.human_attribute_name(attribute_name)
+    end
+
+    # en.activerecord.attribute_values.class_name.attribute_name
+    # Example I18n lookup: en.activerecord.attribute_values.user.state
     def human_value(attribute_name)
       value = send(attribute_name)
       if value.present?
